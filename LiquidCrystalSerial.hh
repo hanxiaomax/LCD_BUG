12bfa2373b78640844e560dd2cede337c31eafe0
lingfeng_linux <linfeng_seu@hotmali.com>
2014-03-29 17:50:19 +0800

    com1

--- LiquidCrystalSerial.hh
+++ LiquidCrystalSerial.hh
@@ -8,43 +8,43 @@
 #include "Pin.hh"
 #include "Configuration.hh"
 
-// commands
-#define LCD_CLEARDISPLAY 0x01
-#define LCD_RETURNHOME 0x02
-#define LCD_ENTRYMODESET 0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT 0x10
-#define LCD_FUNCTIONSET 0x20
-#define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
+// commands 指令
+#define LCD_CLEARDISPLAY 0x01//请屏
+#define LCD_RETURNHOME 0x02//返回？
+#define LCD_ENTRYMODESET 0x04//进入模式设置
+#define LCD_DISPLAYCONTROL 0x08//显示设置
+#define LCD_CURSORSHIFT 0x10//光标移动？
+#define LCD_FUNCTIONSET 0x20//函数设置
+#define LCD_SETCGRAMADDR 0x40//设置自定义字型(cgram)地址(addr)
+#define LCD_SETDDRAMADDR 0x80//设置显示数据随机存储器(ddram)地址
 
-// flags for display entry mode
+// flags for display entry mode  显示输入模式标志？
 #define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
-// flags for display on/off control
-#define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
-#define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
-#define LCD_BLINKOFF 0x00
+// flags for display on/off control 显示开关控制标志
+#define LCD_DISPLAYON 0x04  //显示on
+#define LCD_DISPLAYOFF 0x00 //显示off
+#define LCD_CURSORON 0x02   //光标on
+#define LCD_CURSOROFF 0x00  //光标off
+#define LCD_BLINKON 0x01    //闪烁on
+#define LCD_BLINKOFF 0x00   //闪烁off
 
-// flags for display/cursor shift
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
-#define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
+// flags for display/cursor shift  显示/光标 的移动
+#define LCD_DISPLAYMOVE 0x08    //显示移动
+#define LCD_CURSORMOVE 0x00     //光标移动
+#define LCD_MOVERIGHT 0x04      //右移
+#define LCD_MOVELEFT 0x00       //左移
 
-// flags for function set
-#define LCD_8BITMODE 0x10
-#define LCD_4BITMODE 0x00
-#define LCD_2LINE 0x08
-#define LCD_1LINE 0x00
-#define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
+// flags for function set   函数设置标志
+#define LCD_8BITMODE 0x10   //8bit模式
+#define LCD_4BITMODE 0x00   //4bit模式
+#define LCD_2LINE 0x08      //2行
+#define LCD_1LINE 0x00      //1行
+#define LCD_5x10DOTS 0x04   //5*10 点阵？
+#define LCD_5x8DOTS 0x00    //5*8 点阵？
 
 // TODO:  make variable names for rs, rw, e places in the output vector
 
